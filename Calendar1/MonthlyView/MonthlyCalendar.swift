8618e59450df87d3581b9f171ee2c648bdc5e93f
heoblitz <wone2287@gmail.com>
2022-09-12 23:16:41 +0900

    [Etc] MonthlyCalendar 리팩토링 및 레이아웃 버그 수정

--- Calendar1/MonthlyView/MonthlyCalendar.swift
+++ Calendar1/MonthlyView/MonthlyCalendar.swift
@@ -7,92 +7,98 @@
 
 import SwiftUI
 
-
 struct MonthlyCalendar: View {
+    private struct MonthData: Hashable {
+        let index: Int
+        let month: Date
+    }
     
-    @EnvironmentObject var dateHolder: DateHolder
+    @State private var calHeight: CGFloat? = nil
+    @State private var calHeights: [Int: CGFloat?] = [:]
+    @State private var pageIndex: Int = 0
+    @State private var monthDatas: [MonthData]
     
-    @State var calHeight: CGFloat? = nil
-    @State var calHeights: [Int: CGFloat?] = [:]
+    private let calendarHelper = CalendarHelper()
+    private let currentDate: Date
     
-    @State var pageIndex: Int = 0
-    @State var numOfBackwardPages: Int = 6
-    @State var numOfForwardPages: Int = 6
-    
+    init() {
+        let currentDate = Date()
+        let monthDatas = (-20...20).map { index in
+            MonthData(index: index, month: CalendarHelper().getMonthAdding(index, to: currentDate))
+        }
+        
+        self.currentDate = currentDate
+        self._monthDatas = State(initialValue: monthDatas)
+    }
     
     var body: some View {
-        
-        let calendarHelper = CalendarHelper()
-        let nowMonth = calendarHelper.getFirstOfMonth(dateHolder.date)
-        
         VStack(spacing: 0) {
             MonthlyCalendarTitle()
-            
             TabView(selection: $pageIndex) {
-                
-                // 현재 이전 월들
-                ForEach(-numOfBackwardPages ... -1, id: \.self) { index in
-                    let month = calendarHelper.getMonthAdding(index, to: nowMonth)
-                    
-                    MonthlyCalendarItem(month: month)
-                        .tag(index)
+                ForEach(self.monthDatas, id: \.self) { monthDate in
+                    MonthlyCalendarItem(month: monthDate.month)
+                        .tag(monthDate.index)
                         .background(
-                            HeightReaderView(index: index, Heights: $calHeights)
-                        )
-                }
-
-                // 현재 월
-                MonthlyCalendarItem(month: nowMonth)
-                    .tag(0)
-                    .background(
-                        HeightReaderView(index: 0, Heights: $calHeights)
-                    )
-                    .onAppear {
-                        // 초기 높이 세팅 (초기에는 TabView의 onChange(of: pageindex) 수정자가 호출되지 않음)
-                        if (pageIndex == 0 && calHeights.keys.contains(0)) {
-                            calHeight = calHeights[0]!
+                            HeightReaderView(index: monthDate.index, Heights: self.$calHeights)
+                        ).onAppear {
+                            // 초기 높이 세팅 (초기에는 TabView의 onChange(of: pageindex) 수정자가 호출되지 않음)
+                            if (pageIndex == 0 && calHeights.keys.contains(0)) {
+                                calHeight = calHeights[0]!
+                            }
                         }
-                    }
-
-                // 현재 이후 월들
-                ForEach(1 ... numOfForwardPages, id: \.self) { index in
-                    let month = calendarHelper.getMonthAdding(index, to: nowMonth)
-
-                    MonthlyCalendarItem(month: month)
-                        .tag(index)
-                        .background(
-                            HeightReaderView(index: index, Heights: $calHeights)
-                        )
                 }
-                
             }
             .tabViewStyle(
                 PageTabViewStyle(indexDisplayMode: .never)
             )
             .onChange(of: pageIndex) { _ in
-                
                 // calHeight 업데이트 (동적 높이 조절)
-                if (calHeights.keys.contains(pageIndex)) {
-                    withAnimation() {
-                        calHeight = calHeights[pageIndex]!
+              
+                // 페이지 확장 처리
+                var isUpdated: Bool = false
+                
+                // TODO: prev append 시 레이아웃 깨짐
+                // 리서칭 후 다시 수정하기 by heoblitz
+                if let firstIndex = self.monthDatas.first?.index, pageIndex == firstIndex {
+                    let insertedMonthDatas = (abs(firstIndex) + 1...abs(firstIndex) + 20).map { index in
+                        MonthData(index: -index, month: CalendarHelper().getMonthAdding(-index, to: currentDate))
+                    }.reversed()
+                    
+                    self.monthDatas = insertedMonthDatas + self.monthDatas
+                    isUpdated = true
+                }
+                
+                if let lastIndex = self.monthDatas.last?.index, pageIndex == lastIndex {
+                    let appendedMonthDatas = (lastIndex + 1...lastIndex + 20).map { index in
+                        MonthData(index: index, month: CalendarHelper().getMonthAdding(index, to: currentDate))
+                    }
+                    self.monthDatas += appendedMonthDatas
+                    isUpdated = true
+                }
+                
+                // MARK: @State 업데이트와 겹치면 height 레이아웃 반영안됨
+                // 따라서 레이아웃 업데이트 미루어줌. by heoblitz
+                guard isUpdated else {
+                    if (calHeights.keys.contains(pageIndex)) {
+                        withAnimation() {
+                            calHeight = calHeights[pageIndex]!
+                        }
+                    }
+                    return
+                }
+    
+                DispatchQueue.main.async {
+                    if (calHeights.keys.contains(pageIndex)) {
+                        withAnimation() {
+                            calHeight = calHeights[pageIndex]!
+                        }
                     }
                 }
-                
-                // 페이지 확장 처리
-                if pageIndex == -numOfBackwardPages {
-                    numOfBackwardPages += 6
-                }
-                if pageIndex == numOfForwardPages {
-                    numOfForwardPages += 6
-                }
-
             }
             .frame(height: calHeight)
-            
         }
     }
 }
-
 
 struct MonthlyCalendar_Previews: PreviewProvider {
     
